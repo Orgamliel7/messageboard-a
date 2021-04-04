909330c5d5defc5298604b7212147ff308d1e5d1
Orgamliel7 <57177424+Orgamliel7@users.noreply.github.com>
2021-04-04 15:51:44 +0300

    Add files via upload
    עדכון לקבצי h&cpp

--- Board.cpp
+++ Board.cpp
@@ -1,4 +1,4 @@
-
+#include <array>
 #include <string>
 #include <vector>
 #include <iterator>
@@ -7,55 +7,79 @@ using namespace std;
 #include "Board.hpp"
 #include "Direction.hpp"
 using ariel::Direction;
-
-
- // עדכון המימד של הלוח
+  
  
- Direction dir; //  הצהרה על משתנה מסוג כיוון 
 
 
 
 
+// מודרניזציה ללוח לאחר כל שינוי בו
 void ariel::Board::modernizeBoundery(unsigned int horizontal, unsigned int vertical, Direction dir, unsigned int len)  //  עדכון גבולות של הלוח
 { // מקבלת אופקי , אנכי, כיוון ואורך של המילה
-    unsigned int lenWithHorizontal; // אפס דיפולטיבית
-    unsigned int lenWithVertical;
+     int lenWithHorizontal = 0; 
+     int lenWithVertical = 0;
     if(dir == Direction::Horizontal) // אם הכיוון אופקי
     {
-        lenWithHorizontal=horizontal+1; //  אורך האופקי שווה לאופקי שהכנסנו בפונקציה פלוס  1 של הנוכחי כי הוא מוסיף רק את האות הראשונה 
-        lenWithVertical=len+vertical; // אורך המילה יתווסף למאונך 
+    
+        lenWithHorizontal=(int)horizontal+1; //  אורך האופקי שווה לאופקי שהכנסנו בפונקציה פלוס  1 של הנוכחי כי הוא מוסיף רק את האות הראשונה 
+         lenWithVertical=(int)(len+vertical); // אורך המילה יתווסף למאונך 
     }
     else if(dir == Direction::Vertical) // אחרת אם הכיוון הוא אנכי
     {
-        lenWithHorizontal=horizontal+len;
-        lenWithVertical=1+vertical;
+        lenWithHorizontal=(int)(horizontal+len);
+        lenWithVertical=1+(int)vertical;
     }
-    if (lenWithHorizontal > horizontal_end) // נבדוק האם גודל האופקי הנוכחי גדול מגודל האופקי האחרון של הלוח
-    {
-        horizontal_end = lenWithHorizontal; // נעדכן את הגודל של הלוח להיות הגודל של האופקי
-    }
-    if (horizontal < horizontal_start) // אם המיקום האופקי שהכנסנו קטן מנקודת ההתחלה של הלוח
-    {
-        horizontal_start = horizontal; // נקודת ההתחלה של הלוח תהיה המיקום האופקי
-    }
-    if (lenWithVertical > vertical_end)  // נעשה אותו דבר עם האנכי
-    {
+
+       if(this->horizontal_start==INT32_MAX&&this->horizontal_end==-1&&this->vertical_start==INT32_MAX&&this->vertical_end==-1) 
+       {
+        this->horizontal_start = horizontal;
+        this->horizontal_end = lenWithHorizontal;
+        this->vertical_start=vertical;
         vertical_end = lenWithVertical;
-    }
-    if (vertical < vertical_start) 
-    {
-        vertical_start = vertical;
-    }
-    board.resize(horizontal_end,"_"); //פונקציה שמעדכנת את הגודל ע"פ מה שקבענו לה, אם גדול תרחיב, אם קטן תקצץ עד אליו. פה זה תמיד יגדיל
+        board.insert(board.begin(),(unsigned int)horizontal_end-horizontal_start,"_");
 
-    for (auto ptr = board.begin(); ptr < board.end(); ptr++) // נעבור על כל הלוח עם מצביע כדי לעשות את זה גם לאנכי
-    {
-        (*ptr).resize(vertical_end,'_'); // נכניס לכל משבצת ריקה קו תחתון כנדרש
-    }
+     }
+     else
+     {
+        if (lenWithHorizontal > horizontal_end)  // נבדוק האם גודל האופקי הנוכחי גדול מגודל האופקי האחרון של הלוח
+        {
+             board.insert(board.end(),  ((unsigned int)lenWithHorizontal - (unsigned int)horizontal_end), "_");
+            horizontal_end = lenWithHorizontal; // נעדכן את הגודל של הלוח להיות הגודל של האופקי
+        }
+        if(horizontal<horizontal_start) // אם המיקום האופקי שהכנסנו קטן מנקודת ההתחלה של הלוח
+        {
+            board.insert(board.begin(), horizontal_start - horizontal, "_");
+            this->horizontal_start=horizontal;
+        }
 
+        if (vertical < vertical_start) 
+        {
+            vertical_start = vertical;
+        }
+        if (lenWithVertical > vertical_end)  // נעשה אותו דבר עם האנכי
+        {
+            this->vertical_end = lenWithVertical;
+        }
+    }
+    //פונקציה שמעדכנת את הגודל ע"פ מה שקבענו לה, אם גדול תרחיב, אם קטן תקצץ עד אליו. פה זה תמיד יגדיל 
+        for (auto ptr = board.begin(); ptr < board.end(); ptr++) // נעבור על כל הלוח עם מצביע כדי לעשות את זה גם לאנכי
+        {
+            (*ptr).resize((unsigned int)vertical_end-vertical_start,'_'); // נכניס לכל משבצת ריקה קו תחתון כנדרש
+        }
 }
-
-
+  // פרסום מודעה מאוזנת
+void ariel::Board::horizontalPosterize(unsigned int horizontal, unsigned int vertical, const std::string& message) 
+{
+    board.at(horizontal-horizontal_start).replace(vertical-vertical_start,message.length(),message);
+}
+  // פרסום מודעה אנכית
+void ariel::Board::verticalPosterize(unsigned int horizontal, unsigned int vertical, std::string message) 
+{
+    for (unsigned int i = 0; i <message.length() ; ++i) 
+    {
+        board.at(horizontal-horizontal_start+i).at(vertical-vertical_start)=message[i];
+    }
+}
 
 // post מפרסמת מודעה חדשה ודורסת אחרת אם קיימת כבר כזו
 void ariel::Board::post(unsigned int horizontal, unsigned int vertical, Direction dir, const string &message)
@@ -64,14 +88,11 @@ void ariel::Board::post(unsigned int horizontal, unsigned int vertical, Directio
     {
         case Direction::Horizontal: //  אם זה אופקי
             modernizeBoundery(horizontal,vertical,dir,message.length()); // נעדכן את גבולות הלוח במקרה הצורך
-            board.at(horizontal).replace(vertical,message.length(),message); // נחליף את הכתוב בלוח במיקום המבוקש שהכנסנו ולכן נביא לו כמות תווים והמודעה עצמה 
+            horizontalPosterize(horizontal,vertical,message); // נחליף את הכתוב בלוח במיקום המבוקש שהכנסנו ולכן נביא לו כמות תווים והמודעה עצמה 
             break;
         case Direction::Vertical:  //  אם זה אנכי
             modernizeBoundery(horizontal,vertical,dir,message.length());
-            for (int i = 0; i < message.length() ; ++i) // נעבור על אורך המודעה 
-             {
-                board.at(horizontal+i).at(vertical)=message[i]; // כל פעם נחליף תו במיקום המבוקש בתו הרצוי של המחרוזת
-             }
+                       verticalPosterize(horizontal,vertical,message);
             break;
         default: // אם לא קיבלנו את אחד משני הכיוונים האפשריים כקלט
             throw out_of_range("please enter one of the two options: Horizontal or Vertical");
@@ -84,41 +105,52 @@ void ariel::Board::post(unsigned int horizontal, unsigned int vertical, Directio
  // פונקציה זו קוראת את ההודעה הכתובה בצורה אופקית ומחזירה אותה כפלט
 string ariel::Board::HorizontalReader(unsigned int horizontal, unsigned int vertical, unsigned int lengthOfWord) 
 {
-    vector<string> str_from_board(1,"_");
+vector<string> str_from_board(1,"_");
     str_from_board.at(0).resize(lengthOfWord,'_');
-    if(horizontal+1>(int)horizontal_end)
+    if(horizontal+1>horizontal_end||horizontal<horizontal_start)
     {
         return str_from_board.at(0);
     }
-    for (unsigned int i=0; i < lengthOfWord && i+vertical<vertical_end; ++i) 
+    for (unsigned int i=0; i < lengthOfWord; ++i) 
     {
-        str_from_board.at(0).at(i)=board.at(horizontal).at(vertical+i);
+        try
+        {
+            str_from_board.at(0).at(i)=board.at(horizontal-horizontal_start).at(vertical-vertical_start+i);
+        } catch (const std::exception& e) 
+        {
+        }
     }
     return str_from_board.at(0);
 }
+
 
   // פונקציה זו קוראת את ההודעה הכתובה בצורה אנכית ומחזירה אותה כפלט
 
 string ariel::Board::VerticalReader(unsigned int horizontal, unsigned int vertical, unsigned int lengthOfWord) 
 {
-    vector<string> str_from_board(1,"_");
+   vector<string> str_from_board(1,"_");
     str_from_board.at(0).resize(lengthOfWord,'_');
-    if(vertical+1>(int)vertical_end)
-    {
-        return str_from_board.at(0);
+    if(vertical+1>(int)vertical_end||vertical<vertical_start){return str_from_board.at(0);
     }
-    for (unsigned int i=0; i < lengthOfWord && i+horizontal<horizontal_end; ++i) 
+    for (unsigned int i=0; i < lengthOfWord && horizontal+i<horizontal_end; ++i) 
     {
-        str_from_board.at(0).at(i)=board.at(horizontal+i).at(vertical);
+        try 
+        {
+            str_from_board.at(0).at(i) = board.at(horizontal-horizontal_start + i).at(vertical-vertical_start);
+        } catch (const std::exception& e) 
+        {
+
+        }
     }
     return str_from_board.at(0);
 }
+
 /**
  פונקציה זו תקרא לאחת מפונקציות העזר ותחזיר את הפלט שלהן, תלוי באיזה כיוון מדובר
  לכן נעביר להן את המיקום האנכי והאופקי, ואת אורך המודעה
  * return the text found in the coordination's, direction and length inputted by the user
  */
-string ariel::Board::read(unsigned int horizontal, unsigned int vertical, Direction d, unsigned int lengthOfWord) 
+string ariel::Board::read(unsigned int horizontal, unsigned int vertical, Direction dir, unsigned int lengthOfWord) 
 {
     switch (dir) 
     {
@@ -133,11 +165,13 @@ string ariel::Board::read(unsigned int horizontal, unsigned int vertical, Direct
 
 void ariel::Board::show()  // צפייה בלוח המודעות
 {
-    // נעבור בלולאה על כל השורות ובתוכן 
-    for (unsigned int i = horizontal_start; i < horizontal_end; ++i) 
-    {
+      for (auto & item : board) // נעבור בלולאה על כל השורות ובתוכן עם פוינטר אוטומטי
+      {
+           cout << string(item) << endl; // בכל אחד מהם נדפיס שורת מודעות בלוח לפי הסדר 
+      }
+}
         // מתוכן נדפיס מתוך כל וקטור אנכי את שורת המודעות הנוכחית ע"י שימוש  בפונקציית הסטרינג שתקבל מיקום איטרטור התחלתי ומיקום סופי 
-        cout << string(board.at(i).substr(vertical_start,vertical_end-vertical_start)) << endl;
+      
         /*
             i= horizontal_start בהתחלה
            new apartment in Haifa! נראה את שורת המודעות הראשונה
@@ -146,5 +180,5 @@ void ariel::Board::show()  // צפייה בלוח המודעות
            new apartment in Haifa! 
            want to learn spanish in one month? call us +972524535
         */
-    }
-}
+    
+
--- Board.hpp
+++ Board.hpp
@@ -8,11 +8,14 @@ namespace ariel
     class Board // private by default 
     {
             vector<string> board; // הלוח יהיה וקטור של מחרוזות שהן המודעות עצמן
-            unsigned int horizontal_start=INT32_MAX;
-            unsigned int horizontal_end=0;
+             unsigned int horizontal_start=INT32_MAX;
+            int horizontal_end=-1;
             unsigned int vertical_start=INT32_MAX;
-            unsigned int vertical_end=0;
+            int vertical_end=-1;
             void modernizeBoundery(unsigned int horizontal, unsigned int vertical,Direction dir, unsigned int len);
+            
+            void horizontalPosterize(unsigned int horizontal,unsigned int vertical,const std::string& message);
+            void verticalPosterize(unsigned int horizontal,unsigned int vertical,std::string message);
             string HorizontalReader(unsigned int horizontal, unsigned int vertical, unsigned int len);
             string VerticalReader(unsigned int horizontal, unsigned int vertical, unsigned int len);
     public:
